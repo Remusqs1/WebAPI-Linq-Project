f0a09fe9f7c9153dbf8eaed0ffd7a439b6ab692d
Ivan Camilo Quintero <ivan.quintero@sixdegreesit.solutions>
2021-06-18 15:32:05 -0500

    Agregar archivos de proyecto.

---
+++ Linq/Controllers/LinqController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Linq.Controllers
+{
+    public class LinqController : ApiController
+    {
+        public string GetStatus(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return "Ok, sin input";
+            else return "Ok";
+        }
+    }
+}
