cf0924bc3be70e6d4c8be31798bc4c59bc5ecfee
Ivan Camilo Quintero <ivan.quintero@sixdegreesit.solutions>
2021-06-18 15:59:31 -0500

    Insert into table

---
+++ Linq.Business/LinqBS.cs
@@ -0,0 +1,17 @@
+﻿using Linq.Common.Entities;
+using Linq.DataAccess;
+
+namespace Linq.Business
+{
+    public class LinqBS
+    {
+        public Result CreateData(string input)
+        {
+            Result output = Result.Error;
+            LinqDA dataAccess = new LinqDA();
+            var result = dataAccess.CreateData(input);
+            if (result == Result.Success) output = Result.Success;
+            return output;
+        }
+    }
+}
